0b4dd82046c32c7efac277d84fc5020e0701fd63
Yuting Huang <38852271+ythuang96@users.noreply.github.com>
2022-08-10 16:54:33 -0700

    Update TCP to use epoll

--- CLib_TCP.c
+++ CLib_TCP.c
@@ -3,13 +3,18 @@
 /*
  * Setup server side for TCP
  * Arguments:
- *   master_socket_ptr: [int *, Output] the master socket descriptor
+ *   master_socket_ptr: [Output] pointer to the master socket descriptor
+ *   epoll_fd_ptr:      [Output] pointer to the epoll file descriptor
+ *   events_monitored:  [Output] array of events that are monitored, with first
+ *                               one being the master socket and the client
+ *                               socket arranged base on client ip address
  * Return:
  *    0: if setup successful
  *   -1: if setup failed
  */
-int tcp_server_setup( int *master_socket_ptr ) {
+int tcp_server_setup( int *master_socket_ptr, int *epoll_fd_ptr, struct epoll_event events_monitored[MAXCLIENTS+1] ) {
   int opt = 1;
+  int i;
   struct sockaddr_in address;
 
   /* Type of socket created */
@@ -39,10 +44,26 @@ int tcp_server_setup( int *master_socket_ptr ) {
   /* Listen
    * puts the server socket in a passive mode, where it waits for the client to
    * approach the server to make a connection.
-   * 10 backlog, defines the maximum length of pending connections for the
-   * master socket */
-  if (listen(*master_socket_ptr, 10) < 0) {
+   * MAXCLIENTS backlog, defines the maximum length of pending connections for
+   * the master socket */
+  if (listen(*master_socket_ptr, MAXCLIENTS) < 0) return -1;
+
+
+  /* Create epoll file descriptor */
+  if ( (*epoll_fd_ptr = epoll_create1(0)) == -1) return -1;
+
+  /* Add master_socket to epoll monitor */
+  events_monitored[0].events = EPOLLIN; /* watch for input events */
+  events_monitored[0].data.fd = *master_socket_ptr;
+  if(epoll_ctl(*epoll_fd_ptr, EPOLL_CTL_ADD, *master_socket_ptr, &events_monitored[0])) {
+    close(*epoll_fd_ptr);
     return -1;
+  }
+
+
+  /* Initialise all client sockets to -1 */
+  for ( i = 1 ; i < MAXCLIENTS+1 ; i++) {
+    events_monitored[i].data.fd = -1;
   }
 
   return 0;
@@ -52,130 +73,98 @@ int tcp_server_setup( int *master_socket_ptr ) {
 /*
  * Montior TCP comm from the server side, run this continuously in a loop
  * Arguments:
- *   master_socket:  [int, Input]
- *                   file ID for master socket
- *   client_sockets: [int, size MAXCLIENTS, Input/Output]
- *                   array of file IDs for the client socket, gets updated by
- *                   this function if new clients connects to the server
+ *   master_socket:    [Input]
+ *                     file ID for master socket
+ *   epoll_fd:         [Input]
+ *                     epoll file descriptor
+ *   events_monitored: [Input/Output]
+ *                     array of file IDs for the client socket, gets updated by
+ *                     this function if new clients connects to the server
  * Return:
  *    0: on success
  *   -1: on failure
  */
-int tcp_server_monitor( int master_socket, int client_sockets[MAXCLIENTS] ) {
-  int i;
+int tcp_server_monitor( int master_socket, int epoll_fd, struct epoll_event events_monitored[MAXCLIENTS+1] ) {
+  int event_count, i;
   /* message length */
-  int valread;
+  int bytes_read;
   /* data buffer */
   char buffer[BUFFERSIZE];
-  /* Set of socket descriptors
-   * which will monitor all the active file descriptors of all the clients
-   * plus that of the main server listening socket.*/
-  fd_set readfds;
   /* temp socket descripters */
-  int sd, max_sd, new_socket;
-  /* time interval for select */
-  struct timeval tv;
-  /* activity from select */
-  int activity;
+  int sd, new_socket;
+
+  /* array_postion in events_monitored */
+  int array_position;
+  /* array to store the active_events */
+  struct epoll_event active_events[MAXCLIENTS];
 
   /* Holds address to new socketets */
   struct sockaddr_in address;
   int addrlen;
   addrlen = sizeof(address);
 
-  /* select time out time */
-  tv.tv_sec = 0;
-  tv.tv_usec = 1000000/TCPUPDATEFREQ;
-
-  /************************** Build the socket set ****************************/
-  FD_ZERO(&readfds);                /* clear the socket set               */
-  FD_SET(master_socket, &readfds);  /* add master socket to set           */
-  max_sd = master_socket;           /* set highest file descriptor number */
-                                    /* need it for the select function    */
-
-  /* Add each client sockets to set */
-  for ( i = 0 ; i < MAXCLIENTS ; i++) {
-    sd = client_sockets[i];
-    /* If valid socket descriptor, then add to read list */
-    if(sd > 0) FD_SET( sd , &readfds);
-    /* Set highest file descriptor number */
-    if(sd > max_sd) max_sd = sd;
-  }
-
 
   /**************************** Monitor Activity ******************************/
-  /* Wait for an activity on one of the sockets, timeout is tv ,
-   * set to update at TCPUPDATEFREQ
-   * After select() has returned, readfds will be cleared of all file
-   * descriptors except for those that are ready for reading.
-   * We will need to rebuild readfds on next loop. */
-  activity = select( max_sd + 1 , &readfds , NULL , NULL , &tv);
-  /* return if select timed out */
-  if (activity == 0) return 0;
-
+  /* Wait for an activity on one of the sockets, timeout is set to update at
+   * TCPUPDATEFREQ */
+  event_count = epoll_wait(epoll_fd, active_events, MAXCLIENTS, 1000/TCPUPDATEFREQ);
   /* Whenever a new client connects, master_socket will be activated and a new
    * fd will be open for that client. We will store its fd in the array
-   * client_sockets and in the next iteration we will add it to the readfds to
-   * monitor for activity from this client.
-   * Similarly, if an old client sends some data, readfds will be activated and
-   * we will check from the list of existing client to see which client has
-   * send the data. */
+   * events_monitored and in the add it to epoll to monitor for activity from
+   * this client.
+   * Similarly, if an old client sends some data, the file descrpitor will
+   * activated. */
 
+  /********************* Loop over all the active events *********************/
+  for (i = 0; i < event_count; i++) {
 
-  /***************************** New Connection *******************************/
-  /* If something happened on the master socket, then its an new connection.
-   * Check with FD_ISSET if master_socket is in the active list */
-  if (FD_ISSET(master_socket, &readfds)) {
-    /* accept new connection */
-    new_socket = accept( master_socket, (struct sockaddr *)&address, \
-      (socklen_t*)&addrlen );
+    if ( active_events[i].data.fd == master_socket ) {
+      /*************************** New Connection *****************************/
+      /* If the master socket is active, then it is an new connection. */
 
-    if ( new_socket < 0 ) return -1;
+      /* accept new connection */
+      new_socket = accept( master_socket, (struct sockaddr *)&address, \
+        (socklen_t*)&addrlen );
 
-    /* Print new connection information */
-    printf("New connection , socket fd is %d , ip is : %s , port : %d\n" , \
-      new_socket , inet_ntoa(address.sin_addr) , ntohs
-          (address.sin_port));
+      if ( new_socket < 0 ) return -1;
 
-    /* Add new socket to array of sockets */
-    for (i = 0; i < MAXCLIENTS; i++) {
-      /* if position is empty */
-      if( client_sockets[i] == 0 ) {
-        client_sockets[i] = new_socket;
-        break;
-      }
+      /* Print new connection information */
+      printf("New connection , socket fd is %d , ip is : %s , port : %d\n" , \
+        new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+
+      /* Add new socket to be monitors */
+      array_position = last_ip_digit(inet_ntoa(address.sin_addr))-MINCLIENTADD+1;
+      events_monitored[array_position].events = EPOLLIN; /* watch for input events */
+      events_monitored[array_position].data.fd = new_socket;
+      epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_socket, &events_monitored[array_position]);
     }
-  }
+    else {
+      /**************************** IO By Client ******************************/
+      /* else it is some IO operation on some client socket */
+      sd = active_events[i].data.fd;
 
-
-  /****************************** IO By Client ********************************/
-  /* else its some IO operation on some other socket */
-  /* loop over all client sockets */
-  for (i = 0; i < MAXCLIENTS; i++) {
-    sd = client_sockets[i];
-
-    /* Check if this clinet is active */
-    if (FD_ISSET( sd , &readfds)) {
       /* Read incomming message and store in buffer,
-       * and return the number of bytes read to valread */
-      valread = read( sd , buffer, BUFFERSIZE);
+       * and return the number of bytes read to bytes_read */
+      bytes_read = read( sd , buffer, BUFFERSIZE);
 
-      if ( valread == 0) {
-        /* If valread is 0, then someone disconnected, get details and print */
+      if ( bytes_read == 0) {
+        /* If valread is 0, then the client disconnected, get details and print */
         getpeername(sd , (struct sockaddr*)&address , \
             (socklen_t*)&addrlen);
         printf("Host disconnected , ip %s , port %d \n" ,
               inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
 
-        /* Close the socket, and remove from client list */
+        /* Remove the client socket from events_monitored, and close the socket */
+        array_position = last_ip_digit(inet_ntoa(address.sin_addr))-MINCLIENTADD+1;
+        events_monitored[array_position].data.fd = -1;
+        epoll_ctl(epoll_fd, EPOLL_CTL_DEL, sd, &events_monitored[array_position]);
         close( sd );
-        client_sockets[i] = 0;
       }
 
       /* Else, a message is sent from the clinet */
       else {
         /* NULL terminate buffer */
-        buffer[valread] = '\0';
+        buffer[bytes_read] = '\0';
         printf("Message from client is: %s\n", buffer);
 
         nsleep(1000000000);
@@ -188,19 +177,34 @@ int tcp_server_monitor( int master_socket, int client_sockets[MAXCLIENTS] ) {
 }
 
 
-
-
-void tcp_server_cleanup( int master_socket , int client_sockets[MAXCLIENTS] ) {
+/*
+ * Cleanup TCP comm from the server side, closes the master socket, client
+ * sockets and the epoll file descriptor
+ * Arguments:
+ *   master_socket:    [Input]
+ *                     file ID for master socket
+ *   epoll_fd:         [Input]
+ *                     epoll file descriptor
+ *   events_monitored: [Input]
+ *                     array of file IDs for the client socket
+ * Return:
+ *   None
+ */
+void tcp_server_cleanup( int master_socket, int epoll_fd, struct epoll_event events_monitored[MAXCLIENTS+1] ) {
   int i;
+
+  /* close client sockets */
+  for ( i = 1; i < MAXCLIENTS+1; i++) {
+    if ( events_monitored[i].data.fd > 0 ) {
+      close( events_monitored[i].data.fd );
+    }
+  }
 
   /* close master_socket */
   close( master_socket );
-  /* close client sockets */
-  for ( i = 0 ; i < MAXCLIENTS ; i++) {
-    if ( client_sockets[i] > 0 ) {
-      close( client_sockets[i] );
-    }
-  }
+
+  /* close epoll file descriptor */
+  close( epoll_fd );
 
   return;
 }
